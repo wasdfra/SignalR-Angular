a6a7c7daff7ce8707e6d491c7df402b7e4b21a28
Francisco Bernardo Azevedo <francisco.bernardoazevedo@altran.com>
2021-07-06 18:06:02 +0100

    Functional WS

--- RealTImeCharts-Client/src/app/services/signal-r.service.ts
+++ RealTImeCharts-Client/src/app/services/signal-r.service.ts
@@ -11,37 +11,32 @@ export class SignalRService {
 
   private hubConnection: signalR.HubConnection;
 
+  token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJIb3NwaXRhbElEIjoiOWNhNzlhMjUtYzM5My00MmEzLWQ2M2QtMDhkOTM3YjQ3YjIwIiwiV2luZ0lEIjoiOWNhNzlhMjUtYzM5My00MmEzLWQ2M2QtMDhkOTM3YjQ3YjIwIiwic3ViIjoiMzVmMzEzNWUtMGIyOS00ZjY0LWI4ZDAtNWViNzRiNjkyYTViIiwibmFtZWlkIjoiMjk5OTk5OTk5IiwianRpIjoiZTk1ZjBiODMtZDhhOS00ODYxLTk2ZTUtYTQxMTEzMzMxOTczIiwibmJmIjoxNjI1NTg3ODQxLCJpYXQiOjE2MjU1ODc4NDEsInJvbGUiOiJNb2RlcmF0b3IiLCJleHAiOjE2MjU1OTUwNDEsImlzcyI6IkNPVklEVHJpYWdlIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3QifQ.noNVecZkYnQHCpGBCKA-mjHt-qk8HwMK0U63Kjj49F0';
+
   public startConnection = () => {
     this.hubConnection = new signalR.HubConnectionBuilder()
-                            .withUrl('https://localhost:8448/api/WaitingList', {
-                              headers: {
-                                // tslint:disable-next-line:max-line-length
-                                'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJIb3NwaXRhbElEIjoiOWNhNzlhMjUtYzM5My00MmEzLWQ2M2QtMDhkOTM3YjQ3YjIwIiwiV2luZ0lEIjoiOWNhNzlhMjUtYzM5My00MmEzLWQ2M2QtMDhkOTM3YjQ3YjIwIiwic3ViIjoiMzVmMzEzNWUtMGIyOS00ZjY0LWI4ZDAtNWViNzRiNjkyYTViIiwibmFtZWlkIjoiMjk5OTk5OTk5IiwianRpIjoiZTk1ZjBiODMtZDhhOS00ODYxLTk2ZTUtYTQxMTEzMzMxOTczIiwibmJmIjoxNjI1NTg3ODQxLCJpYXQiOjE2MjU1ODc4NDEsInJvbGUiOiJNb2RlcmF0b3IiLCJleHAiOjE2MjU1OTUwNDEsImlzcyI6IkNPVklEVHJpYWdlIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3QifQ.noNVecZkYnQHCpGBCKA-mjHt-qk8HwMK0U63Kjj49F0'
-                              }
-                            })
-                            .build();
+      .withUrl('https://localhost:8448/api/WaitingList', {
+        accessTokenFactory: () => this.token
+        }
+      )
+      .build();
 
     this.hubConnection
       .start()
       .then(() => console.log('Connection started'))
       .catch(err => console.log('Error while starting connection: ' + err));
-  }
 
-  public addTransferChartDataListener = () => {
-    this.hubConnection.on('transferchartdata', (data) => {
+    this.hubConnection.on('WaitingList', (data) => {
       this.data = data;
       console.log(data);
     });
-  }
-
-  public broadcastChartData = () => {
-    this.hubConnection.invoke('broadcastchartdata', this.data)
-    .catch(err => console.error(err));
-  }
-
-  public addBroadcastChartDataListener = () => {
-    this.hubConnection.on('broadcastchartdata', (data) => {
-      this.bradcastedData = data;
-    })
+    this.hubConnection.on('AddPatient', (data) => {
+      this.data = data;
+      console.log(data);
+    });
+    this.hubConnection.on('RemovePatient', (data) => {
+      this.data = data;
+      console.log(data);
+    });
   }
 }
